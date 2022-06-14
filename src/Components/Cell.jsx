cc07a8166d71fd042da9273c283b760b1cf28d13
Vbif322 <hxdfue@yandex.ru>
2022-06-14 12:30:15 +0300

    Тестирую проверку

--- src/Components/Cell.jsx
+++ src/Components/Cell.jsx
@@ -2,17 +2,26 @@ import React from 'react'
 import './Cell.css'
 
 
+
 function Cell( { num, row, col, setInputNum, Arr } ) {
-    
+    let CellClass = 'CellInput'
+
+    /* if (Arr[row][col] === 1) {
+        CellClass = 'CellInputWrong'
+    } */
+
    const onInputHandler = (event) => {
     setInputNum(event)
     Arr[row][col] = Number(event.target.value)
-       
-   }
+
+    }
+
+
+    
 
     return (
     <div className='Cell'>
-        <input maxLength="1" onChange = {onInputHandler} defaultValue ={num} className='CellInput' />            
+        <input maxLength="1" onChange = {onInputHandler} defaultValue ={num} className={CellClass} />            
     </div>
   )
 }
